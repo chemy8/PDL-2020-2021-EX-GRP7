77ac707d637034391b5d1c9f3560f5062366bfba
Mariem <mariem.rouiissi@gmail.com>
2020-11-04 21:55:15 +0100

    clean code

--- src/Url.py
+++ src/Url.py
@@ -3,19 +3,12 @@ import re
 
 class Url:
 
-
-    url = "https://www.guru99.com/python-regular-expressions-complete-tutorial.html"
-
     # Verify that is a wikipedia page
     def estWikiPage(url):
         pattern = re.compile("https://(fr|en).wikipedia.org/(w|wiki)/")
         return pattern.match(url)
 
-    print(estWikiPage(url))
-
     # Verify that the titre not empty
     def estTitreValid(titre):
         pattern = re.compile("(.|\s)*\S(.|\s)*")
         return pattern.match(titre)
-
-    print(estTitreValid(" "))
