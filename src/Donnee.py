0ef77cc268a625f412da63f6c85d301685cdc5a3
Mariem <mariem.rouiissi@gmail.com>
2020-11-04 18:20:52 +0100

    creer urls file & generaliser le code

--- src/Donnee.py
+++ src/Donnee.py
@@ -3,34 +3,52 @@ import requests
 import pandas as pd
 
 
-# with <th> & class wikitable
-# url = "https://en.wikipedia.org/wiki/Comparison_between_U.S._states_and_countries_by_GDP_(PPP)"
-# url= "https://en.wikipedia.org/wiki/Comparison_between_Ido_and_Novial"
-
-# without <th> & without class
-# url="https://en.wikipedia.org/wiki/Comparison_of_Canadian-tax_preparation_software_for_personal_use"
-
 class Donnee:
+    urls = ["https://en.wikipedia.org/wiki/Comparison_of_iSCSI_targets",
+            "https://en.wikipedia.org/wiki/Comparison_between_U.S._states_and_countries_by_GDP_(PPP)",
+            "https://en.wikipedia.org/wiki/Comparison_between_Ido_and_Novial",
+            "https://en.wikipedia.org/wiki/Comparison_of_HTML_parsers",
+            "https://en.wikipedia.org/wiki/Comparison_of_MIDI_standards",
+            "https://en.wikipedia.org/wiki/Comparison_of_Power_Management_Software_Suites",
+            "https://en.wikipedia.org/wiki/Comparison_of_S.M.A.R.T._tools",
+            "https://en.wikipedia.org/wiki/Comparison_of_DNS_blacklists",
+            "https://en.wikipedia.org/wiki/Comparison_of_dance_pad_video_games"]
+    with open('Urls') as file:
 
-    urls = ["https://en.wikipedia.org/wiki/Comparison_between_Ido_and_Novial",
-            "https://en.wikipedia.org/wiki/Comparison_between_U.S._states_and_countries_by_GDP_(PPP)"]
-    for j in urls:
+        for url in urls:
+            # print(url)
+            html_content = requests.get(url).text
 
-        html_content = requests.get(j).text
+            soup = BeautifulSoup(html_content, "html.parser")
+            print(html_content)
 
-        soup = BeautifulSoup(html_content, "html.parser")
-        # print(soup.prettify())
+            if soup.find("table", {'class': 'wikitable'}) is not None:
 
-        table = soup.find("table", {'class': 'wikitable'}).tbody
-        rows = table.find_all("tr")
-        columns = [v.text.replace('\n', '') for v in rows[0].find_all("th")]
+                table = soup.find("table", {'class': 'wikitable'})
+                rows = table.find_all("tr")
 
-        df = pd.DataFrame(columns=columns)
+                columns = [v.text.replace('\n', '') for v in rows[0].find_all("th")]
 
-        for i in range(1, len(rows)):
-            tds = rows[i].find_all("td")
-            values = [td.text.replace('\n', '') for td in tds]
-            # print(j, values)
-            df = df.append(pd.Series(values, index=columns), ignore_index=True)
-            title = soup.title.text
-            df.to_csv(r'D:\workspace\PDL-2020-2021-EX-GRP7' + '\\' + title + '.csv', index=False)
+                df = pd.DataFrame(columns=columns)
+
+                ths = []
+                tds = []
+
+                for i in range(1, len(rows)):
+                    tds = rows[i].find_all("td")
+                    ths = rows[i].find_all("th")
+                    data = tds + ths
+                    values = [td.text.replace('\n', '') for td in data]
+                    if len(data) == len(columns):
+                        df = df.append(pd.Series(values, index=columns), ignore_index=True)
+                    else:
+                        for j in range(0, len(data)):
+                            length = j + 1
+                        for k in range(length + 1, len(columns) + 1):
+                            values.append('')
+                    df = df.append(pd.Series(values, index=columns), ignore_index=True)
+
+                title = soup.title.text
+                df.to_csv(r'D:\workspace\PDL-2020-2021-EX-GRP7' + '\\' + title + '.csv', index=False)
+            else:
+                print("Aucun tableau present dans la page")
---
+++ src/Url.py
@@ -0,0 +1,6 @@
+with open("../Urls") as file_in:
+    lines = []
+    for line in file_in:
+        lines.append(line)
+        print(line)
+
