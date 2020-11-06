024fd91b4fc8ad4efc75dff53d31effee44f302c
Mariem <mariem.rouiissi@gmail.com>
2020-11-06 19:01:34 +0100

    change url

--- src/wikiExtractMain.py
+++ src/wikiExtractMain.py
@@ -3,7 +3,7 @@ from src.Url import Url
 
 class wikiExtractMain:
     # exemple for test URL
-    url = "https://www.guru99.com/python-regular-expressions-complete-tutorial.html"
+    url = "https://annuel2.framapad.org/p/r.35d94f38fd3cbed4e5664cab60aad1f7"
     title = "test title !"
     validURL = Url.estTitreValid(title) and Url.estWikiPage(url)
     print(validURL)
