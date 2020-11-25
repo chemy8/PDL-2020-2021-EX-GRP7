93642835a5fa5908a134e6f0f38f91f23760a1a4
Mariem <mariem.rouiissi@gmail.com>
2020-11-25 18:39:41 +0100

    run java program from python

---
+++ src/comparator.py
@@ -0,0 +1,5 @@
+import subprocess
+
+subprocess.call(['java', '-jar',
+                 'D:\workspace\PDL_2020-2021_GR7\\target\WikipediaMatrix-0.0.1-SNAPSHOT.jar',
+                 'https://en.wikipedia.org/wiki/Comparison_of_S.M.A.R.T._tools', 'H'])
