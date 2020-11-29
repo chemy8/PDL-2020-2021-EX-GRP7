3f89a0d7454fa4faad6689d8e4fcd8cb3918cbfa
Mariem <mariem.rouiissi@gmail.com>
2020-11-29 22:27:29 +0100

    table editable & fix style

--- comparator_interface.py
+++ comparator_interface.py
@@ -1,15 +1,26 @@
 import flask
+from bs4 import BeautifulSoup
 from flask import Flask, render_template, request
 import subprocess
-
 from src.Donnee import Donnee
+import pandas as pd
 
 app = Flask(__name__)
+
+df = pd.read_csv("./output/Comparison of S.M.A.R.T. tools - Wikipedia.csv", header=None)
+heading = list()
+data = list()
+for j in range(0, len(df.values[0])):
+    heading.append(df.values[0][j])
+#print(heading)
+for i in range(1, len(df.values)):
+    data.append(df.values[i])
+#print(data)
 
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    return render_template("index.html", heading=heading, data=data)
 
 
 @app.route("/urlForm/", methods=['POST', 'GET'])
@@ -24,9 +35,14 @@ def move_forward():
                              url, ext])
         else:
             Donnee.extraire(url)
-
-    print(flask.request.form)
+    #print(flask.request.form)
     return render_template('index.html', forward_message=forward_message)
+
+
+@app.route("/validForm/", methods=['POST', 'GET'])
+def validation():
+    Donnee.extract_table("http://127.0.0.1:5000/")
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
